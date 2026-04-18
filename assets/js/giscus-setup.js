88c090f79cad496e7e960af8c225dbfe9b61fe73
saipavan4 <57193478+saipavan4@users.noreply.github.com>
2026-04-18 07:54:39 +0000

    Deploying to gh-pages from @ saipavan4/saipavan4.github.io@33dcc8379e5ff227f3ffc4d516aef80d1b447334 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "saipavan4/Webpage",
+    "data-repo": "saipavan4/saipavan4.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
